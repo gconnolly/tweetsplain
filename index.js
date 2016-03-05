a22998c2e0110543ff7ad3ddeb61894a6a202fca
geneconnolly <gene.connolly@meltwater.com>
2016-03-05 13:04:52 -0500

    parse twitter id from url

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
+const bodyParser = require("body-parser")
+const twitterParse = require('twitter-url-parser')
+
+app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
-  console.log(req.body)
+  console.log(twitterParse(req.body.link).id)
   res.end()
 })
 
