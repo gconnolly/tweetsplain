1a906833d295438165f51ae3eaab1f4b5447e96c
geneconnolly <gene.connolly@meltwater.com>
2016-03-16 16:15:19 -0400

    adjust tweet limit

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function tweetsplain (req, res, tweetTheResult) {
             console.log(tweet.text)
             twitter.search(
               {
-                q: tweet.text.substring(0, 100),
+                q: tweet.text.substring(0, 50),
                 max_id: bigInt(twitterId).minus(1).toString()
               },
               access.token,
