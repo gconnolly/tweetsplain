1aaf7bb27100835389dd8afd9d80047bcd42abae
geneconnolly <gene.connolly@meltwater.com>
2016-03-16 16:21:48 -0400

    tweak twitter character limit

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function tweetsplain (req, res, tweetTheResult) {
             console.log(tweet.text)
             twitter.search(
               {
-                q: tweet.text.substring(0, 75),
+                q: tweet.text.substring(0, 90),
                 max_id: bigInt(twitterId).minus(1).toString()
               },
               access.token,
