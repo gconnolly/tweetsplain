8f8356a858eeaaea40f77c33430024919baf167a
geneconnolly <gene.connolly@meltwater.com>
2016-03-16 16:31:54 -0400

    encode parens

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function tweetsplain (req, res, tweetTheResult) {
             console.log(tweet.text)
             twitter.search(
               {
-                q: tweet.text,
+                q: tweet.text.replaceAll('(', '%28').replaceAll(')', '%29'),
                 max_id: bigInt(twitterId).minus(1).toString()
               },
               access.token,
