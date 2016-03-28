8ee68e6b159c5ace21da8c73e99b2d00171242a2
geneconnolly <gene.connolly@meltwater.com>
2016-03-27 21:45:48 -0400

    console logging

--- index.js
+++ index.js
@@ -157,6 +157,7 @@ function tweetsplain (req, res, tweetTheResult) {
                                   console.log(error)
                                   res.end()
                                 } else {
+                                  console.log(body)
                                   if (body && body.items && body.items[0]) {
                                     console.log('@' + req.body.username + ' ' + body.items[0].link)
                                     res.send('@' + req.body.username + ' ' + body.items[0].link)
