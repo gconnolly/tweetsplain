26a767b1b87b4a2934cd799b02aedeb7c13fa780
geneconnolly <gene.connolly@meltwater.com>
2016-03-30 14:17:28 -0400

    order hn results by date

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ function tweetsplain (req, res, tweetTheResult) {
                           uri: process.env.ALGOLIA_URL,
                           method: 'POST',
                           json: {
-                            'params': 'query=%22' + encodeURIComponent(tweet.text) + '%22'
+                            'params': 'query=%22' + encodeURIComponent(tweet.text) + '%22&sort=byDate'
                           }
                         },
                         (error, response, body) => {
@@ -103,7 +103,7 @@ function tweetsplain (req, res, tweetTheResult) {
                             reject(error)
                           } else {
                             if (body && body.hits && body.hits[0]) {
-                              resolve('@' + req.body.username + ' https://hn.algolia.com/?query=' + encodeURIComponent(tweet.text) + '&type=all ' + body.hits[0].story_url)
+                              resolve('@' + req.body.username + ' https://hn.algolia.com/?query=' + encodeURIComponent(tweet.text) + '&type=all&sort=byDate ' + body.hits[0].story_url)
                             } else {
                               console.log('no matching hn comment')
                               request(
